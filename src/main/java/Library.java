6570b1accd8b9c4b9d8764b778e6fdada14ac027
Jamie <jamienolan93@hotmail.co.uk>
2022-02-08 16:31:02 +0000

    added title, author and genre to book

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> shelf;
+    private int capacity;
+
+    public Library(){
+        this.shelf = new ArrayList<>();
+        this.capacity = 5;
+    }
+
+
 }
