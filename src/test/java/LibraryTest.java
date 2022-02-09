e849eb0c23a2dd3cb36254defd176ab32a840147
Jamie <jamienolan93@hotmail.co.uk>
2022-02-09 07:35:06 +0000

    add book when capacity full test passed

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -8,6 +8,7 @@ public class LibraryTest {
     Book book1;
     Book book2;
     Book book3;
+    Book book4;
 
     @Before
     public void before(){
@@ -15,6 +16,7 @@ public class LibraryTest {
         book1 = new Book("Hola", "James Nolan", "Autobiography");
         book2 = new Book("Coding for Dummies", "J Script", "Educational");
         book3 = new Book("100 Quiz Questions", "Bill Chanter", "Non-fiction");
+        book4 = new Book("Too Big for the Shelf", "Anon", "Tragedy");
     }
 
     @Test
@@ -31,6 +33,15 @@ public class LibraryTest {
     @Test
     public void hasCapacity(){
         assertEquals(3, library.getCapacity());
+    }
+
+    @Test
+    public void cantAddWhenFull(){
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        assertEquals(3, library.getBookCount());
     }
 
 }
