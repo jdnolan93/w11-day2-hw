48ce5bf3392b02423f4ea56400ae6edb5aabaf73
Jamie <jamienolan93@hotmail.co.uk>
2022-02-09 07:38:22 +0000

    added book test setup

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,11 @@
+import org.junit.Before;
+
 public class BookTest {
+
+    Book book;
+
+    @Before
+    public void setup(){
+        book = new Book("Hola", "James Nolan", "Autobiography");
+    }
 }
